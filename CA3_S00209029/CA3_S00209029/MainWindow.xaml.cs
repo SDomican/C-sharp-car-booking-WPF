7152c6ba8f0fd7042d56054121cbad705276b9c8
Stephen Domican <57364979+SDomican@users.noreply.github.com>
2021-05-05 21:51:53 +0100

    05052021

--- CA3_S00209029/CA3_S00209029/MainWindow.xaml.cs
+++ CA3_S00209029/CA3_S00209029/MainWindow.xaml.cs
@@ -31,28 +31,14 @@ namespace CA3_S00209029
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            dtpStartDate.SelectedDate = DateTime.Now;
+            dtpEndDate.SelectedDate = DateTime.Now;
 
             carImage.Source = new BitmapImage(new Uri("/Images/vw.png", UriKind.Relative));
 
 
-            /*var query = (from c in db.Cars
-                         select new
-                         {
-                             Id = c.Id,
-                             Make = c.Make,
-                             Model = c.Model,
-                             Size = c.Size
-                         }).AsEnumerable().Select(item => new Car()
-                         {
-                             Make = item.Make,
-                             Model = item.Model,
-                             Size = item.Size
-                         });
-            
-            */
             var query = from c in db.Cars
-                        select c.ToString();
+                        select c;
                         
 
 
@@ -62,8 +48,7 @@ namespace CA3_S00209029
             carTypes.Add("All");
             carTypes = carTypes.OrderBy(type => type).ToList();
 
-            //lsbxCars.ItemsSource = query.ToList().OrderBy(car => car.Make)
-            //.ThenBy(car => car.Model);
+
 
             lsbxCars.ItemsSource = query.ToList();
 
@@ -100,23 +85,54 @@ namespace CA3_S00209029
         private void lsbxCars_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
-            MessageBox.Show(lsbxCars.SelectedItem.ToString());
-
-
-            /*int selectedCarId = Convert.ToInt32(lsbxCars.SelectedItem);
-               
-
-            var query = from c in db.Cars
-                       where c.Id == selectedCarId
-                       select c;
-
-            txblSelectedCar.Text = query.ToString();
-            */
+            UpdateSelectedCarTable();
+            
         }
 
         private void btnBook_Click(object sender, RoutedEventArgs e)
         {
+            Bookings bookingWindow = new Bookings();
+            bookingWindow.Owner = this;
+            bookingWindow.ShowDialog();
+        }
 
+        private void dtpStartDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (txblSelectedCar.Text.Length > 0)
+                UpdateSelectedCarTable();
+
+        }
+
+        private void dtpEndDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (txblSelectedCar.Text.Length > 0)
+                UpdateSelectedCarTable();
+        }
+
+        private void UpdateSelectedCarTable()
+        {
+            Car selectedCar = lsbxCars.SelectedItem as Car;
+
+            if (selectedCar == null)
+                return;
+
+            int selectedCarId = Convert.ToInt32(selectedCar.Id);
+
+
+            var query = from c in db.Cars
+                        where c.Id == selectedCarId
+                        select new
+                        {
+                            Id = c.Id,
+                            Make = c.Make,
+                            Model = c.Model,
+                            Size = c.Size
+                        };
+
+
+
+            txblSelectedCar.Text = String.Format("Car Id: {0}\nMake: {1}\nModel: {2}\nSize: {3}\nRental Date: {4}\nReturn Date: {5}", query.FirstOrDefault().Id, query.FirstOrDefault().Make, query.FirstOrDefault().Model, query.FirstOrDefault().Size, dtpStartDate.SelectedDate.Value.ToShortDateString(), dtpEndDate.SelectedDate.Value.ToShortDateString());
         }
     }
 }
